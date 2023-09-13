a00077f8e014384ba6a0c2b8d86e88c9d96a2696
TonansisCeja <142059457+TonansisCeja@users.noreply.github.com>
2023-09-12 20:26:13 -0700

    Add files via upload
    This is the Javascript file that displays "Hello World" when the website runs on the browser using an
    alert box.

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+// the hello world program displayed in an alert box over the current window
+let greet="Hello, World";
+alert(greet);
+
+
+//prints hello world to the console.
+console.log('Hello, World!');
